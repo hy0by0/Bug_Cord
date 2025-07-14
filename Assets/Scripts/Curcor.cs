8b42d6a43c550fa1aa312a5d7f3e8c463e911e2b
藤原虎太郎 <kotaro20041212@gmail.com>
2025-07-14 19:01:36 +0900

    OK

--- Assets/Scripts/Curcor.cs
+++ Assets/Scripts/Curcor.cs
@@ -26,7 +26,7 @@ public class Curcor : MonoBehaviour
     public bool shoot_reload_Flag = true;
     public bool shoot_allow = true;
 
-    float moveSpeed = 50f;
+    float moveSpeed = 20f;
 
     // --- シーン遷移関連---
     //遷移フラグ
